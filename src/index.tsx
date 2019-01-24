8098f812404e03ec517378946e143740ca2489f1
alsotang <alsotang@gmail.com>
2019-01-24 19:21:13 +0800

    rewrite with mobx-react-lite

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,28 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {observable} from 'mobx';
-import {observer} from 'mobx-react';
-import DevTools from 'mobx-react-devtools';
+import { observer, useObservable } from 'mobx-react-lite';
 
-class AppState {
-    @observable timer = 0;
-
-    constructor() {
-        setInterval(() => {
-            this.timer += 1;
-        }, 1000);
+const TimerView = observer((props) => {
+  const timer = useObservable({
+    value: 0,
+    reset() {
+      timer.value = 0
     }
+  })
 
-    resetTimer() {
-        this.timer = 0;
-    }
-}
+  React.useEffect(() => {
+    setInterval(() => {
+      timer.value++;
+    }, 1000)
+  }, [])
 
-@observer
-class TimerView extends React.Component<{appState: AppState}, {}> {
-    render() {
-        return (
-            <div>
-                <button onClick={this.onReset}>
-                    Seconds passed: {this.props.appState.timer}
-                </button>
-                <DevTools />
-            </div>
-        );
-     }
+  return (
+    <div>
+      <button onClick={() => timer.reset()}>
+        Seconds passed: {timer.value}
+      </button>
+    </div>
+  )
+})
 
-     onReset = () => {
-         this.props.appState.resetTimer();
-     }
-};
-
-const appState = new AppState();
-ReactDOM.render(<TimerView appState={appState} />, document.getElementById('root'));
+ReactDOM.render(<TimerView />, document.getElementById('root'));
